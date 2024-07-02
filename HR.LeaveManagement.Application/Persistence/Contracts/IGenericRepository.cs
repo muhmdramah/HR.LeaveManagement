04b26aa7bae3aab70b48da0068580005082f72df
Mohammed Rammah <90036566+muhmdramah@users.noreply.github.com>
2024-07-02 06:36:51 +0300

    Add project files.

---
+++ HR.LeaveManagement.Application/Persistence/Contracts/IGenericRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace HR.LeaveManagement.Application.Persistence.Contracts
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        Task<T> GetAsync(int id);
+        Task<IReadOnlyList<T>> GetAllAsync();
+        Task<T> AddAsync(T entity);
+        Task<T> UpdateAsync(T entity);
+        Task<T> DeleteAsync(T entity);
+    }
+}
