fef0fd20f7adc32a1bd834ecf5d4d6da630f2959
Mohammed Rammah <90036566+muhmdramah@users.noreply.github.com>
2024-07-03 03:04:20 +0300

    Implement read operations for LeaveType using MediatR and CQRS

--- HR.LeaveManagement.Application/ApplicationServicesRegistration.cs
+++ HR.LeaveManagement.Application/ApplicationServicesRegistration.cs
@@ -1,14 +1,18 @@
-﻿using HR.LeaveManagement.Application.Profiles;
+﻿using MediatR;
 using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
 
 namespace HR.LeaveManagement.Application
 {
     public static class ApplicationServicesRegistration
     {
-        public static void ConfigureApplicationServices(this IServiceCollection services)
+        public static IServiceCollection ConfigureApplicationServices(this IServiceCollection services)
         {
-            //services.AddAutoMapper(Assembly.GetExecutingAssembly());
-            services.AddAutoMapper(typeof(MappingProfile));
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            //services.AddAutoMapper(typeof(MappingProfile));
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+
+            return services;
         }
     }
 }
