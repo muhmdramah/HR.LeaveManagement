a4d6645881fd98ef6e5e8f2b4d32b751c4391e27
Mohammed Rammah <90036566+muhmdramah@users.noreply.github.com>
2024-07-03 04:24:17 +0300

    Implement read operations for LeaveRequest using MediatR and CQRS

---
+++ HR.LeaveManagement.Application/Features/LeaveRequest/Handlers/Queries/GetLeaveRequestDetailRequestHandler.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using HR.LeaveManagement.Application.DTOs.LeaveRequest;
+using HR.LeaveManagement.Application.Features.LeaveRequest.Requests.Queries;
+using HR.LeaveManagement.Application.Persistence.Contracts;
+using MediatR;
+
+namespace HR.LeaveManagement.Application.Features.LeaveRequest.Handlers.Queries
+{
+    public class GetLeaveRequestDetailRequestHandler :
+        IRequestHandler<GetLeaveRequestDetailRequest, LeaveRequestDto>
+    {
+        private readonly ILeaveRequestRepository _leaveRequestRepository;
+        private readonly ILeaveTypeRepository _leaveTypeRepository;
+        private readonly IMapper _mapper;
+
+        public GetLeaveRequestDetailRequestHandler(ILeaveRequestRepository leaveRequestRepository,
+            ILeaveTypeRepository leaveTypeRepository,
+            IMapper mapper)
+        {
+            _leaveRequestRepository = leaveRequestRepository;
+            _leaveTypeRepository = leaveTypeRepository,
+            _mapper = mapper;
+        }
+        public async Task<LeaveRequestDto> Handle(GetLeaveRequestDetailRequest request, CancellationToken cancellationToken)
+        {
+            var leaveRequest = await _leaveRequestRepository.GetLeaveRequestWithDetails(request.Id);
+            return _mapper.Map<LeaveRequestDto>(leaveRequest);
+        }
+    }
+}
---
+++ HR.LeaveManagement.Application/Features/LeaveRequest/Handlers/Queries/GetLeaveRequestListRequestHandler.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using HR.LeaveManagement.Application.DTOs.LeaveRequest;
+using HR.LeaveManagement.Application.Features.LeaveRequest.Requests.Queries;
+using HR.LeaveManagement.Application.Persistence.Contracts;
+using MediatR;
+
+namespace HR.LeaveManagement.Application.Features.LeaveRequest.Handlers.Queries
+{
+    public class GetLeaveRequestListRequestHandler :
+        IRequestHandler<GetLeaveRequestListRequest, List<LeaveRequestDto>>
+    {
+        private readonly ILeaveRequestRepository _leaveRequestRepository;
+        private readonly IMapper _mapper;
+
+        public GetLeaveRequestListRequestHandler(ILeaveRequestRepository leaveRequestRepository,
+            IMapper mapper)
+        {
+            _leaveRequestRepository = leaveRequestRepository;
+            _mapper = mapper;
+        }
+        public async Task<List<LeaveRequestDto>> Handle(GetLeaveRequestListRequest request, CancellationToken cancellationToken)
+        {
+            var leaveRequests = await _leaveRequestRepository.GetLeaveRequestsWithDetails();
+            return _mapper.Map<List<LeaveRequestDto>>(leaveRequests);
+        }
+    }
+}
