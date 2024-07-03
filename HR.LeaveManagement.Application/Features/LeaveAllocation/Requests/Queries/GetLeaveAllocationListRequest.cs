deb68f0dfe4651585c66e0ab94297f0ed9a50962
Mohammed Rammah <90036566+muhmdramah@users.noreply.github.com>
2024-07-03 04:09:55 +0300

    Implement read operations for LeaveAllocation using MediatR and CQRS

---
+++ HR.LeaveManagement.Application/Features/LeaveAllocation/Requests/Queries/GetLeaveAllocationListRequest.cs
@@ -0,0 +1,9 @@
+﻿using HR.LeaveManagement.Application.DTOs;
+using MediatR;
+
+namespace HR.LeaveManagement.Application.Features.LeaveAllocation.Requests.Queries
+{
+    public class GetLeaveAllocationListRequest : IRequest<List<LeaveAllocationDto>>
+    {
+    }
+}
