dca9b1afd963762c6f84fb5a3df2f8da2edcc171
muhmdramah <mohammedrammahux@gmail.com>
2024-07-04 05:59:41 +0300

    Implement Delete operation for LeaveRequest using MediatR and CQRS

---
+++ HR.LeaveManagement.Application/Features/LeaveRequests/Handlers/Commands/DeleteLeaveRequestCommandHandler.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using HR.LeaveManagement.Application.Features.LeaveRequests.Requests.Commands;
+using HR.LeaveManagement.Application.Persistence.Contracts;
+using MediatR;
+
+namespace HR.LeaveManagement.Application.Features.LeaveRequests.Handlers.Commands
+{
+    public class DeleteLeaveRequestCommandHandler :
+        IRequestHandler<DeleteLeaveRequestCommand>
+    {
+        private readonly ILeaveRequestRepository _leaveRequestRepository;
+        private readonly IMapper _mapper;
+
+        public DeleteLeaveRequestCommandHandler(ILeaveRequestRepository leaveRequestRepository,
+            IMapper mapper)
+        {
+            _leaveRequestRepository = leaveRequestRepository;
+            _mapper = mapper;
+        }
+
+        public Task<Unit> Handle(DeleteLeaveRequestCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
