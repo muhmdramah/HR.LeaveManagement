4462bf38e6f799f7183e947b5d594346ec5f22ec
Mohammed Rammah <mohammedrammahux@gmail.com>
2024-07-04 05:40:10 +0300

    Implement Update operation for LeaveRequest using MediatR and CQRS

---
+++ HR.LeaveManagement.Application/Features/LeaveRequests/Handlers/Commands/UpdateLeaveRequestCommandHandler.cs
@@ -0,0 +1,41 @@
+﻿using AutoMapper;
+using HR.LeaveManagement.Application.Features.LeaveRequests.Requests.Commands;
+using HR.LeaveManagement.Application.Persistence.Contracts;
+using MediatR;
+
+namespace HR.LeaveManagement.Application.Features.LeaveRequests.Handlers.Commands
+{
+    public class UpdateLeaveRequestCommandHandler :
+        IRequestHandler<UpdateLeaveRequestCommand, Unit>
+    {
+        private readonly ILeaveRequestRepository _leaveRequestRepository;
+        private readonly IMapper _mapper;
+
+        public UpdateLeaveRequestCommandHandler(ILeaveRequestRepository leaveRequestRepository,
+            IMapper mapper)
+        {
+            _leaveRequestRepository = leaveRequestRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<Unit> Handle(UpdateLeaveRequestCommand request, CancellationToken cancellationToken)
+        {
+            var leaveRequest = await _leaveRequestRepository.GetAsync(request.Id);
+
+            if (request.LeaveRequestDto is not null)
+            {
+
+                _mapper.Map(request.LeaveRequestDto, leaveRequest);
+
+                await _leaveRequestRepository.UpdateAsync(leaveRequest);
+            }
+            else if (request.ChangeLeaveRequestApprovalDto is not null)
+            {
+                await _leaveRequestRepository
+                    .ChangeApprovalStatus(leaveRequest, request.ChangeLeaveRequestApprovalDto.Approved);
+            }
+
+            return Unit.Value;
+        }
+    }
+}
