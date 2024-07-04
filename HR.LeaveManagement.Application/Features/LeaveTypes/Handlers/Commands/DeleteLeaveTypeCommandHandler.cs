a84734f097a3e920d2bb0904cf92806fe833e12d
muhmdramah <mohammedrammahux@gmail.com>
2024-07-04 05:51:12 +0300

    Implement Delete operation for LeaveType using MediatR and CQRS

---
+++ HR.LeaveManagement.Application/Features/LeaveTypes/Handlers/Commands/DeleteLeaveTypeCommandHandler.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using HR.LeaveManagement.Application.Features.LeaveTypes.Requests.Commands;
+using HR.LeaveManagement.Application.Persistence.Contracts;
+using MediatR;
+
+namespace HR.LeaveManagement.Application.Features.LeaveTypes.Handlers.Commands
+{
+    public class DeleteLeaveTypeCommandHandler :
+        IRequestHandler<DeleteLeaveTypeCommand>
+    {
+        private readonly ILeaveTypeRepository _leaveTypeRepository;
+        private readonly IMapper _mapper;
+
+        public DeleteLeaveTypeCommandHandler(ILeaveTypeRepository leaveTypeRepository,
+            IMapper mapper)
+        {
+            _leaveTypeRepository = leaveTypeRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<Unit> Handle(DeleteLeaveTypeCommand request, CancellationToken cancellationToken)
+        {
+            var leaveType = await _leaveTypeRepository.GetAsync(request.Id);
+
+            await _leaveTypeRepository.DeleteAsync(leaveType);
+
+            return Unit.Value;
+        }
+    }
+}
