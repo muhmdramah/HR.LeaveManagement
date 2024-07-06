652679b024ec64a19c9ef30375a9c502b46888b9
muhmdramah <mohammedrammah83@gmail.com>
2024-07-06 04:07:01 +0300

    Add Persistence project and HRLeaveManagementDbContext class

---
+++ Persistence/HRLeaveManagementDbContext.cs
@@ -0,0 +1,38 @@
+﻿using HR.LeaveManagement.Domain;
+using HR.LeaveManagement.Domain.Common;
+using Microsoft.EntityFrameworkCore;
+
+namespace HR.LeaveManagement.Persistence
+{
+    public class HRLeaveManagementDbContext : DbContext
+    {
+        public HRLeaveManagementDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(HRLeaveManagementDbContext).Assembly);
+        }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            foreach (var entity in ChangeTracker.Entries<BaseDomainEntity>())
+            {
+                entity.Entity.LastModifiedDate = DateTime.Now;
+
+                if (entity.State == EntityState.Added)
+                {
+                    entity.Entity.DateCreated = DateTime.Now;
+                }
+            }
+
+
+            return base.SaveChangesAsync(cancellationToken);
+        }
+
+        public DbSet<LeaveAllocation> LeaveAllocations { get; set; }
+        public DbSet<LeaveRequest> LeaveRequests { get; set; }
+        public DbSet<LeaveType> LeaveTypes { get; set; }
+    }
+}
