f04a6884084f6e2fdb4ea4e8e42ec6edcf0c7b37
muhmdramah <mohammedrammah83@gmail.com>
2024-07-07 05:07:16 +0300

    Add Api project

--- HR.LeaveManagement.Domain/Common/BaseDomainEntity.cs
+++ HR.LeaveManagement.Domain/Common/BaseDomainEntity.cs
@@ -4,8 +4,8 @@
     {
         public int Id { get; set; }
         public DateTime DateCreated { get; set; }
-        public string CreatedBy { get; set; }
+        public string? CreatedBy { get; set; }
         public DateTime LastModifiedDate { get; set; }
-        public string LastModifiedBy { get; set; }
+        public string? LastModifiedBy { get; set; }
     }
 }
---
+++ Persistence/LeaveManagementDbContextFactor.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace HR.LeaveManagement.Persistence
+{
+    public class LeaveManagementDbContextFactor :
+        IDesignTimeDbContextFactory<HRLeaveManagementDbContext>
+    {
+        public HRLeaveManagementDbContext CreateDbContext(string[] args)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var builder = new DbContextOptionsBuilder<HRLeaveManagementDbContext>();
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+            builder.UseSqlServer(connectionString);
+
+            return new HRLeaveManagementDbContext(builder.Options);
+        }
+    }
+}
