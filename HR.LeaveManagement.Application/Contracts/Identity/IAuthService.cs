ec71f4d7ddaaf6c294dbaf14d233fb8c62a100bf
muhmdramah <mohammedrammah83@gmail.com>
2024-07-14 19:01:57 +0300

    fixing a bug in IAuthService contract

--- HR.LeaveManagement.Application/Contracts/Identity/IAuthService.cs
+++ HR.LeaveManagement.Application/Contracts/Identity/IAuthService.cs
@@ -5,6 +5,6 @@ namespace HR.LeaveManagement.Application.Contracts.Identity
     public interface IAuthService
     {
         Task<AuthResponse> Login(AuthRequest request);
-        Task<RegistrationResponse> Register(RegistrationRequest request);
+        Task<RegistrationResponse> Register(RegistrationRequest requests);
     }
 }
